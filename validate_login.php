df1d08dea7404d275a6767f25697c85b5207ec89
Calvin Moore <c1moore@ufl.edu>
2014-03-12 13:40:19 -0400

    validate_login.php functioning
    For the most part, running fine.  I haven't tested if it can check if
    the right password is entered yet (we need a sign up page for that).

--- validate_login.php
+++ validate_login.php
@@ -1,14 +1,15 @@
+#!/usr/local/bin/php
+
 <?php
-	$conn = pg_connect('user=cmoore host=postgres.cise.ufl.edu dbname=ghosttalk password=calvin#1');
+	
+	$conn = pg_connect("user=cmoore host=postgres.cise.ufl.edu dbname=ghosttalk password=calvin#1");
 	$query = sprintf("SELECT password FROM GTUser WHERE username='%s'",
-		pg_escape_string($_POST(['Username'])));
+		pg_escape_string($_POST['Username']));
 	$result = pg_fetch_assoc(pg_query($conn, $query));
 	
-	if($row && crypt($_POST(['Password']), $row['password']) == $result['password']) {
-		
+	if($result && crypt($_POST['Password'], $result['password']) == $result['password']) {
+		header('Location: http://www.google.com/');
 	} else {
-		$doc = new DomDocument;
-		$doc->Load('SignIn.html');
-		getElementById("Error").write("Username and/or password incorrect.");
+		header('Location: http://cise.ufl.edu/~cmoore');
 	}
 ?>
