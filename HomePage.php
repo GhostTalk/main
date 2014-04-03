f960384597d1629bea1e37488ec0cf751ad41ccb
dkotwica <domk123@gmail.com>
2014-04-03 13:48:54 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -14,6 +14,8 @@
 	<script src='script.js'></script>
 <script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
 <script src="http://thecodeplayer.com/uploads/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="//code.jquery.com/jquery-1.9.1.js"></script>
+<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 <script>
 $(document).ready(function(){
 	$("#accordian h3").click(function(){
@@ -26,18 +28,32 @@ $(document).ready(function(){
 	})
 })
 </script>
+<script>
+  $(function() {
+    $("#tabs").tabs();
+  });
+</script>
 </head>
 <body>
 <div id ="header">
 <h2>Ghost Talk</h2>
 </div>
-<div id="right" style="overflow:auto">
-        <div id="tabs">
+<div id="tabs">
         <ul>
-            <li>Inbox</li>
-            <li>Send</li>
-            <li>Sent</li>
+            <li><a href="#tab1"><span class="icon-envelope"></span>Inbox</a></li>
+            <li><a href="#tab2"><span class ="icon-mail-forward"></span>Send</a></li>
+            <li><a href="#tab3"><span class ="icon-folder-open"></span>Sent</a></li>
         </ul>
+        <div id = "right" style="overflow:auto">
+            <div id = "tab1">
+            <p>wtf mate</p>
+            </div>
+            <div id = "tab2">
+            <p>NO YOU</p>
+            </div>
+            <div id = "tab3">
+            <p>Getting real tired of your shit html</p>
+            </div>
         </div>
 </div>
 <div id="accordian">
@@ -45,8 +61,7 @@ $(document).ready(function(){
 		<li>
 			<h3>Home</h3>
 		</li>
-		<!-- we will keep this LI open by default -->
-		<li class="active">
+		<li>
 			<h3>Friends List</h3>
 		</li>
 		<li>
