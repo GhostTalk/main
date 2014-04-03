58d482b0ea917b8d29821758e00756874a322c75
christian122333 <cargueta@ufl.edu>
2014-04-03 14:07:34 -0400

    Create formvalid.js

---
+++ formvalid.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	$('#form').sumbit(function(event){
+			
+			
+			var formData = {
+				'username'		:$('input[name=username]').val();
+				'email'			:$('input[name=email]').val();
+				'password'		:$('input[name=password]').val();
+				'Rpassword'		:$('input[name=RPassword]').val();
+				'first'			:$('input[name=First]').val();
+				'last'			:$('input[name=Last]').val();
+				'gender'		:$('input[name=gender]').val();
+			};
+			
+			$.ajax({
+					type   		: 'POST',
+					url	   		: 'process.php',
+					data   		: 'formData',
+					dataType 	: 'json'
+			})
+					.done(function(data){
+					
+						console.log(data);
+						
+						if(! data.success){
+									if(data.errors.Username){
+										$('#Username').append('<div class ="error">' + data.errors.Username + '</div>');
+									
+									}
+									
+									if(data.errors.Email){
+										$('#Email').append('<div class ="error">' + data.errors.Email + '</div>');
+									}
+									
+									if(data.errors.password){
+										$('#Password').append('<div class ="error">' + data.errors.password + '</div>');
+									}
+									
+									if(data.errors.passwordMatch){
+										$('#RPassword').append('<div class ="error">' + data.errors.passwordMatch + '</div>');
+									}
+									
+									if(data.errors.First){
+										$('#First').append('<div class ="error">' + data.errors.First + '</div>');
+									}
+									
+									if(data.errors.Last){
+										$('#Last').append('<div class ="error">' + data.errors.Last + '</div>');
+									}
+									
+									if(data.errors.gender){
+										$('#gender').append('<div class ="error">' + data.errors.gender + '</div>');
+									}
+							}
+							
+						else{
+						
+								$('#form').append('<div>' + data.message + '</div>');
+								alert('success');
+							}
+					
+					});
+						.fail(function(data){
+								console.log(data);
+							
+						});
+						
+						
+						event.preventDefault();
+					});
+	});
+				
