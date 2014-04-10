625d56200c3f4c35fa109cbc1a6ca4cc8fd9e791
Calvin Moore <c1moore@ufl.edu>
2014-04-10 15:02:48 -0400

    Drop down for Groups Functions
    Updates needs to increase functionality of the groups dropdown.

--- HomePage.php
+++ HomePage.php
@@ -1,18 +1,10 @@
 #!/usr/local/bin/php
 
-<?php
-	session_start();
-	if(!isset($_SESSION['Username']))
-		header('Location: http://cise.ufl.edu/~cmoore');
-		exit();
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
-	<title>Home Page</title>
+		<title>Home Page</title>
 		<link rel='stylesheet' href='HomePage.css'/>
-		<script src='script.js'></script>
 		<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
 		<script src="http://thecodeplayer.com/uploads/js/jquery-1.7.1.min.js" type="text/javascript"></script>
 		<script src="//code.jquery.com/jquery-1.9.1.js"></script>
@@ -36,6 +28,14 @@
 		</script>
 	</head>
 	<body>
+		<?php
+			session_start();
+			if(!isset($_SESSION['Username'])) {
+				echo '<script>window.location.href="http://cise.ufl.edu/~cmoore";</script>';
+				exit();
+			}
+		?>
+		
 		<div id ="header">
 			<h2>Ghost Talk</h2>
 		</div>
@@ -66,29 +66,45 @@
 					<h3>Friends List</h3>
 				</li>
 				<li>
-					<h3>Groups</h3>
-					<ul>
-						<li>
-							<h3>Home</h3>
-						</li>
-						<li>
-							<h3>Friends List</h3>
-						</li>
-						<li>
-							<h3>Groups</h3>
-							<ul>
-								<li><a href="#">School</a></li>
-								<li><a href="#">Work</a></li>
-								<li><a href="#">People</a></li>
-							</ul>
-						</li>
-						<li>
-							<h3>Search</h3>
-						</li>
-						<li>
-							<h3>Requests</h3>
-						</li>
+					<h3 id="Groups">Groups</h3>
+					<ul id="GroupsList">
+						<!--<li><a href="#">School</a></li>
+						<li><a href="#">Work</a></li>
+						<li><a href="#">People</a></li>-->
+						<script>
+							$(document).ready(function() {
+								$("#Groups").on("click", function(e) {									
+									$.ajax({
+										type		: 'POST',
+										url			: 'get_groups.php',
+										dataType	: 'json'
+									})
+									
+									.done(function(result) {
+										$('#GroupsList').empty();
+										
+										var counter=0;
+										$.each(result, function(index, value) {
+											$('#GroupsList').append(value.name);
+										});
+									})
+									
+									.fail(function() {
+										$('#GroupsList').empty();
+										$('#GroupsList').append('Error loading groups.');
+									});
+								});
+							});
+						</script>
 					</ul>
+				</li>
+				<li>
+					<h3>Search</h3>
+				</li>
+				<li>
+					<h3>Requests</h3>
+				</li>
+			</ul>
 		</div>
 	</body>
 </html>
---
+++ get_groups.php
@@ -0,0 +1,11 @@
+#!/usr/local/bin/php
+
+<?php
+	session_start();
+	$query = sprintf("SELECT name FROM %s_groups WHERE name<>'Friends';",
+		$_SESSION['Username']);
+	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
+	
+	$result = pg_query($conn, $query);
+	echo json_encode($result = pg_fetch_all($result));
+?>
