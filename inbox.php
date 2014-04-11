31c06a1ace89eeb4d78c51d63981e635c423396e
Calvin Moore <c1moore@ufl.edu>
2014-04-11 12:44:29 -0400

    Update to inbox.php and mainpage.php
    Both of these are functioning as expected at this point.

--- inbox.php
+++ inbox.php
@@ -1,22 +1,22 @@
+#!/usr/local/bin/php
+
 <?php
-session_start();
-$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
+	session_start();
+	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
 
-$user = 'Christian123';//$user = $_SESSION['user'];
-if(isset($_POST['inbox'])) {
-$query = sprintf("SELECT * FROM %s_messages_received",$user);
-$result = pg_query($conn, $query);
-//Loop through all rows in query and echo their values into the inbox div
-$counter = 0;
-$row = pg_fetch_row($result, $counter);
-while($row)
-{
-  $message = "<div class='messagebox'> <img class='save' src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png' width='100' height='100'><p id='sender'>".$row[creatorusername]."</p><p id='posttime'>".$row[posttime]"</p><div class ='dialog'><p id='body'>message 1</p></div></div>";
-  $counter++;
-  $row = pg_fetch_row($result, $counter);
-}
-
-	  echo json_encode($message);
-  
+	$user = 'Christian123';		//$user = $_SESSION['user'];
+	$query = sprintf("SELECT * FROM %s_messages_received",
+		$user);
+	$result = pg_query($conn, $query);
 	
+	$message = array();
+	$counter = 0;
+	
+	//Loop through all rows in query and echo their values into the inbox div
+	while($row = pg_fetch_assoc($result)) {
+		$message[$counter] = "<div class='messagebox'> <img class='save' src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png' width='100' height='100'><p id='sender'>".$row['creatorusername']."</p><p id='posttime'>".$row['posttime']."</p><div class ='dialog'><p id='body'>message 1</p></div></div>";
+		$counter++;
+	}
+
+	echo json_encode($message);
 ?>
--- mainpage.php
+++ mainpage.php
@@ -2,149 +2,141 @@
 
 <!DOCTYPE html>
 <html>
-<head>
-	<link rel='stylesheet' href='HomePage.css'/>
-	<script src='script.js'></script>
-<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
-<script src="http://thecodeplayer.com/uploads/js/jquery-1.7.1.min.js" type="text/javascript"></script>
-<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
-<script src="//code.jquery.com/jquery-1.9.1.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
-<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
-<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-<script>
-//append all html content formed by PHP into the id specified using AJAX
-/*	$.ajax({url:"inbox.php"}).done(function(message){
-		$("#tab1").append(message);
-		});
-*/
+	<head>
+		<link rel='stylesheet' href='HomePage.css'/>
+		<script src='script.js'></script>
+		<script src="http://thecodeplayer.com/uploads/js/prefixfree-1.0.7.js" type="text/javascript" type="text/javascript"></script>
+		<script src="http://thecodeplayer.com/uploads/js/jquery-1.7.1.min.js" type="text/javascript"></script>
+		<link href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/themes/base/jquery-ui.css" rel="stylesheet" type="text/css"/>
+		<script src="//code.jquery.com/jquery-1.9.1.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
+		<script src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+		<script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+		<script>
+			//append all html content formed by PHP into the id specified using AJAX
+			/**$.ajax({url:"inbox.php"}).done(function(message){
+				$("#tab1").append(message);
+			});**/
+
 			$.ajax({
 				url:"inbox.php",
 				type: "POST",
-				dataType: "JSON",
-				success: successC
-				});
+				dataType: "json"
+				})
 				
-				function successC(response){
+				.done(function(response){
 					console.log("Message was deleted");
 					$.each(response, function(index, value) {
 						$('#tab1').append(value);
+						//$('#tab1').append("<div><p><strong>Cool</strong></p></div>");
 					});
-				}	
-				
-</script>
-<script>
-$(document).ready(function(){
-		$("#accordian h3").click(function(){
-			$("#accordian ul ul").slideUp();
-			
-			if(!$(this).next().is(":visible"))
-			{
-				$(this).next().slideDown();
-			}
-		});
-		
-	jQuery(document).ajaxComplete(function () {
-	 $(".messagebox").click(function(e) {
-		var box = $(this);
-		var sentBy = $(this).find("#sender").html();
-		var time = $(this).find("#posttime").html();
-		$(this).find(".dialog").dialog({
-			title:'dialog',
-			width: 600,
-			height: 400,
-			modal:true,
-			resizable:false,
-			draggable:false,
-		
-			close: function (ev, ui) {
-				var dataObject = {sender: sentBy, posttime: time};
-				
-				$.ajax({
-				url:"deleteMessage.php",
-				type: "POST",
-				data: dataObject,
-				dataType: "JSON",
-				success: successCall
 				});
-				
-				
-				
-				$(this).dialog("destroy");
-				$(this).remove();
-				$(box).remove();
-			}
+		</script>
+		<script>
+			$(document).ready(function(){
+				$("#accordian h3").click(function(){
+					$("#accordian ul ul").slideUp();
 			
-			});
+					if(!$(this).next().is(":visible")) {
+						$(this).next().slideDown();
+					}
+				});
 		
-		});
-	});
-	
-		setTimeout(function(){
-			$('.dialog').dialog('close');
-		}, 3000);
+				jQuery(document).ajaxComplete(function () {
+					$(".messagebox").click(function(e) {
+						var box = $(this);
+						var sentBy = $(this).find("#sender").html();
+						var time = $(this).find("#posttime").html();
+						$(this).find(".dialog").dialog({
+							title:'dialog',
+							width: 600,
+							height: 400,
+							modal:true,
+							resizable:false,
+							draggable:false,
 		
-		
-		
-		function successCall(){
-			console.log("Message was deleted");
-		
-		}	
-		
-});
-</script>
-<script>
-  $(function() {
-    $("#tabs").tabs();
-  });
-</script>
-</head>
-<body>
-<div id ="header">
-<h2>Ghost Talk</h2>
-</div>
-<div id="tabs">
-        <ul>
-            <li><a href="#tab1"><span class="icon-envelope"></span>Inbox</a></li>
-            <li><a href="#tab2"><span class ="icon-mail-forward"></span>Send</a></li>
-            <li><a href="#tab3"><span class ="icon-folder-open"></span>Sent</a></li>
-        </ul>
-        <div id = "right" style="overflow:auto">
-            <div id = "tab1">
+							close: function (ev, ui) {
+								var dataObject = {sender: sentBy, posttime: time};
 				
-            </div>
-            <div id = "tab2">
-            <p>NO YOU</p>
-            </div>
-            <div id = "tab3">
-            <p>Getting real tired of your shit html</p>
-            </div>
-        </div>
-</div>
-<div id="accordian">
-	<ul>
-		<li>
-			<h3>Home</h3>
-		</li>
-		<li>
-			<h3>Friends List</h3>
-		</li>
-		<li>
-			<h3>Groups</h3>
+								$.ajax({
+									url:"deleteMessage.php",
+									type: "POST",
+									data: dataObject,
+									dataType: "JSON",
+									success: successCall
+								});
+				
+								$(this).dialog("destroy");
+								$(this).remove();
+								$(box).remove();
+							}
+						});
+					});
+				});
+	
+				setTimeout(function(){
+					$('.dialog').dialog('close');
+				}, 3000);
+				
+				function successCall(){
+					console.log("Message was deleted");
+				}	
+		
+			});
+		</script>
+		<script>
+			$(function() {
+				$("#tabs").tabs();
+			});
+		</script>
+	</head>
+	<body>
+		<div id ="header">
+			<h2>Ghost Talk</h2>
+		</div>
+		<div id="tabs">
 			<ul>
-				<li><a href="#">School</a></li>
-				<li><a href="#">Work</a></li>
-				<li><a href="#">People</a></li>
+				<li><a href="#tab1"><span class="icon-envelope"></span>Inbox</a></li>
+				<li><a href="#tab2"><span class ="icon-mail-forward"></span>Send</a></li>
+				<li><a href="#tab3"><span class ="icon-folder-open"></span>Sent</a></li>
 			</ul>
-		</li>
-		<li>
-			<h3>Search</h3>
-		</li>
-		<li>
-			<h3>Requests</h3>
-		</li>
-	</ul>
-</div>
-
-</body>
+			
+			<div id = "right" style="overflow:auto">
+					<div id = "tab1">
+				</div>
+            
+				<div id = "tab2">
+					<p>NO YOU</p>
+				</div>
+            
+				<div id = "tab3">
+					<p>Getting real tired of your shit html</p>
+				</div>
+			</div>
+		</div>
+		<div id="accordian">
+			<ul>
+				<li>
+					<h3>Home</h3>
+				</li>
+				<li>
+					<h3>Friends List</h3>
+				</li>
+				<li>
+					<h3>Groups</h3>
+					<ul>
+						<li><a href="#">School</a></li>
+						<li><a href="#">Work</a></li>
+						<li><a href="#">People</a></li>
+					</ul>
+				</li>
+				<li>
+					<h3>Search</h3>
+				</li>
+				<li>
+					<h3>Requests</h3>
+				</li>
+			</ul>
+		</div>
+	</body>
 </html>
