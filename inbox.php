3eb8ff27455004b30f565b15c951380f50d4f405
Calvin Moore <c1moore@ufl.edu>
2014-04-13 10:36:14 -0400

    Update inbox.php
    Added a div to the boxes so it would show properly in seconds instead of
    milliseconds.

--- inbox.php
+++ inbox.php
@@ -14,7 +14,12 @@
 	
 	//Loop through all rows in query and echo their values into the inbox div
 	while($row = pg_fetch_assoc($result)) {
-		$message[$counter] = "<div class='messagebox'> <img class='save' src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png' width='100' height='100'><p id='sender'>".$row['creatorusername']."</p><p id='posttime'>".$row['posttime']."</p><p id='expirationtime'>".$row['expirationtime']."</p><div class ='dialog' style='display:none'>".$row['body']."</div></div>";
+		if($row['expirationtime']) {
+			$expires = $row['expirationtime']/1000;
+			$expires = $expires . ' sec';
+		} else
+			$expires = 'Unlimited';
+		$message[$counter] = "<div class='messagebox'> <img class='save' src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png' width='100' height='100'><p id='sender'>".$row['creatorusername']."</p><p id='posttime'>".$row['posttime']."</p><div id='expires'>".$expires."</div><p id='expirationtime' style='display:none'>".$row['expirationtime']."</p><div class ='dialog' style='display:none'>".$row['body']."</div></div>";
 		$counter++;
 	}
 
