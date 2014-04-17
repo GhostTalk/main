dd41d330c5823e30772d02f6fd3343b919e5a9e5
christian122333 <cargueta@ufl.edu>
2014-04-17 13:57:20 -0400

    Create outbox.php

---
+++ outbox.php
@@ -0,0 +1,20 @@
+<?php
+	session_start();
+	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
+
+	$user = $_SESSION['Username'];
+	$query = sprintf("SELECT * FROM %s_messages_sent ORDER BY postTime DESC",
+		$user);
+	$result = pg_query($conn, $query);
+
+	$message = array();
+	$counter = 0;
+
+	//Loop through all rows in query and echo their values into the inbox div
+	while($row = pg_fetch_assoc($result)) {
+	$message[$counter] = "<div class='messagebox'><img class='save' src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png' width='100' height='100'><p id='sender'>".$row['creatorusername']."</p><p id='posttime'>".substr($row['posttime'], 0, 16)."</p><p id='postt' style='display:none'>".$row['posttime']."</p><p id='expirationtime' style='display:none'>".$row['expirationtime']."</p><div class ='dialog' style='display:none'><div id='timer'><p></p></div>".$row['body']."</div></div>";
+	$counter++;
+	}
+
+	echo json_encode($message);
+?>
