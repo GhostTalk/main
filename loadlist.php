689726eebf368850aeef9f0c56078911d4eb47c6
Calvin Moore <c1moore@ufl.edu>
2014-04-18 08:21:23 -0400

    Update loadlist.php
    Made loadlist.php work properly with UF server and made it so it would
    get the username from the session.

--- loadlist.php
+++ loadlist.php
@@ -1,18 +1,21 @@
+#!/usr/local/bin/php
+
 <?php
-	//session_start();
+	session_start();
 	
 	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
 	$query = sprintf("SELECT memberUsername FROM Groups WHERE name='Friends' AND creatorUsername='%s'",
-		pg_escape_string("cmoore"));//$_SESSION['Username']));
+		pg_escape_string($_SESSION['Username']));
 
 	$result = pg_query($conn, $query);
 	$message = array();
-	$counter = 0;
+	$message[0] = "<option></option>";
+	$counter = 1;
 		
-		while ($row = pg_fetch_row($result)) {
+	while ($row = pg_fetch_row($result)) {
 		$message[$counter] = "<option>$row[0]</option>";
-		$counter++;
- 	  
-		}
+		$counter++;  
+	}
+	
  	 echo json_encode($message);
 ?>
