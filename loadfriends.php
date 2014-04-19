66cf5c30ebefc06c773109a9036fe41102ee31d4
Calvin Moore <c1moore@ufl.edu>
2014-04-19 08:42:14 -0400

    Update loadfriends.php
    Fixed the queries to the database.

--- loadfriends.php
+++ loadfriends.php
@@ -1,16 +1,17 @@
+#!/usr/local/bin/php
+
 <?php
 	session_start();
 	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
-
 	$user = $_SESSION['Username'];
-	$query = sprintf("SELECT * FROM groups WHERE name=friends AND creatorusername = $user" , $user);
+	$query = sprintf("SELECT * FROM groups WHERE name='Friends' AND creatorusername = '%s'" , pg_escape_string($user));
 	$result = pg_query($conn, $query);
 	
 	$list = array();
 	$counter = 0;
 	
 	while($row = pg_fetch_assoc($result)) {
-		$list[$counter] = "<div>".$row['friend']."</div>";
+		$list[$counter] = "<div>".$row['memberusername']."</div>";
 		$counter++;
 	}
 
