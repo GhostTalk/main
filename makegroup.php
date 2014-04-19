b2f9788e7d924903ad4ffd9c2d5a60c39b4d8ddc
christian122333 <cargueta@ufl.edu>
2014-04-18 21:06:06 -0400

    Create makegroup.php

---
+++ makegroup.php
@@ -0,0 +1,21 @@
+<?php
+	session_start();
+	$user = $_SESSION['Username'];
+	$friend = array();
+	$friend = $_POST['friendlist'];
+	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
+	$counter = 0;
+	while($counter < count($friend)){
+		if(!empty($friend[$counter])){
+			$query = sprintf("INSERT INTO groups(name,creatorusername,memberusername) VALUES ('%s', '%s', '%s')",
+				pg_escape_string($groupname),
+				pg_escape_string($user),
+				pg_escape_string($friend[$counter]));
+				$result = pg_query($conn, $query);
+				
+			}
+		$counter++;
+	}
+			
+
+?>
