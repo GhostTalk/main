9fdf3b34eff07a9a7470a1c2607b3df3af97a546
christian122333 <cargueta@ufl.edu>
2014-04-20 00:36:05 -0400

    Create addmember.php

---
+++ addmember.php
@@ -0,0 +1,24 @@
+#!/usr/local/bin/php
+<?php
+	session_start();
+	$user = $_SESSION['Username'];
+	$friend = array();
+	$friend = $_POST['friendlist'];
+	$group = $_POST['group']
+	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
+	$counter = 0;
+		while($counter < count($friend)){
+			if(!empty($friend[$counter])){
+			$query = sprintf("INSERT INTO groups(name,creatorusername,memberusername) VALUES ('%s', '%s', '%s')",
+			pg_escape_string($group),
+			pg_escape_string($user),
+			pg_escape_string($friend[$counter]));
+			$result = pg_query($conn, $query);
+
+		}
+			$counter++;
+	}
+
+
+
+?>
