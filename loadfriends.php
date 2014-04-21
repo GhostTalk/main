d9436de16a61b76974145b9bf75f7b8eab7ea0b3
Calvin Moore <c1moore@ufl.edu>
2014-04-21 03:57:17 -0400

    Update loadfriends.php
    Corrected query and extended amount of informtion shown about your
    friends.

--- loadfriends.php
+++ loadfriends.php
@@ -4,14 +4,15 @@
 	session_start();
 	$conn = pg_connect("host=postgres.cise.ufl.edu user=cmoore dbname=ghosttalk password=calvin#1");
 	$user = $_SESSION['Username'];
-	$query = sprintf("SELECT * FROM groups WHERE name='Friends' AND creatorusername = '%s'" , pg_escape_string($user));
+	$query = sprintf("SELECT * FROM %s_groups WHERE name='Friends'" , pg_escape_string($user));
+	
 	$result = pg_query($conn, $query);
 	
 	$list = array();
 	$counter = 0;
 	
 	while($row = pg_fetch_assoc($result)) {
-		$list[$counter] = "<div>".$row['memberusername']."</div>";
+		$list[$counter] = "<div><img src='".$row['picture']."' width='100px' height='100px' /><h4>".$row['username']."</h4><p class=fname>".$row['firstname']. " " .$row['lastname']."</p></div>";
 		$counter++;
 	}
 
