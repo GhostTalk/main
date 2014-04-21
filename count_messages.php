29d0c5c97d9c23be84144fd02049517f9411e6ba
Calvin Moore <c1moore@ufl.edu>
2014-04-21 02:29:28 -0400

    Update count_messages.php and count_requests.php
    Properly counts messages and requests according to current
    specifications of the website.

--- count_messages.php
+++ count_messages.php
@@ -1,11 +1,13 @@
 #!/usr/local/bin/php
 
 <?php
-	$query = sprintf("SELECT COUNT(*) FROM %s_messages_received WHERE read=false;",
-		$_POST['Username']);
+	session_start();
+	
+	$query = sprintf("SELECT COUNT(*) AS mess_count FROM %s_messages_received WHERE read=false;",
+		$_SESSION['Username']);
 	$conn = pg_connect('host=postgres.cise.ufl.edu user=cmoore password=calvin#1 dbname=ghosttalk');
 	
 	$result = pg_query($conn, $query);
 	
-	echo $result;
+	echo json_encode(pg_fetch_assoc($result));
 ?>
--- count_requests.php
+++ count_requests.php
@@ -1,11 +1,13 @@
 #!/usr/local/bin/php
 
 <?php
-	$query = sprintf("SELECT COUNT(*) FROM %s_requests_received;",
-		$_POST['Username']);
+	session_start();
+	
+	$query = sprintf("SELECT COUNT(*) as count_requ FROM %s_requests_received;",
+		$_SESSION['Username']);
 	$conn = pg_connect('host=postgres.cise.ufl.edu user=cmoore password=calvin#1 dbname=ghosttalk');
 	
 	$result = pg_query($conn, $query);
 	
-	echo $result;
+	echo json_encode(pg_fetch_assoc($result));
 ?>
