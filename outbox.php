414fb6077f154267029fa3a587306b4681e402fe
Calvin Moore <c1moore@ufl.edu>
2014-04-21 03:59:24 -0400

    Update outbox.php
    If the expiration time is NULL say "No expiration" instead of 0.

--- outbox.php
+++ outbox.php
@@ -15,6 +15,8 @@
 	//Loop through all rows in query and echo their values into the inbox div
 	while($row = pg_fetch_assoc($result)) {
 		$expires = $row['expiration']/1000;
+		if($expires == 0)
+			$expires = "No expiration";
 		if($row['pic'] == "f")
 			$message[$counter] = "<div class='sentbox'><img class='save' src='".$row['picture']."' width='100' height='100'><p id='sender'>".$row['receiverusername']."</p><p id='posttime'>".substr($row['posttime'], 0, 16)."</p><p id='expirationtime'>".$expires."</p><div class ='dialog'>".$row['body']."</div></div>";
 		else
